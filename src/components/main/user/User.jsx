ce01cc3608654a53ffc78d5a12056194b77f5d6d
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-05-16 19:45:11 +0300

    feat: add adaptive layout

--- src/components/main/user/User.jsx
+++ src/components/main/user/User.jsx
@@ -9,11 +9,15 @@ const User = ({ user }) => {
 				className={s.avatar}
 				alt="avatar"
 			/>
-			<h3 className={s.username}>{user.name}</h3>
-			<a href={user.html_url} className={s.nickname} target="_blank" rel="noopener noreferrer">{user.login}</a>
-			<div className={s.follow}>
-				<div className={s.followers}>{user.followers < 1000 ? user.followers : (user.followers / 1000).toFixed(1) + 'k'} followers</div>
-				<div className={s.following}>{user.following < 1000 ? user.following : (user.following / 1000).toFixed(1) + 'k'} following</div>
+			<div className={s.about}>
+				<div className={s.person}>
+					<h3 className={s.username}>{user.name}</h3>
+					<a href={user.html_url} className={s.nickname} target="_blank" rel="noopener noreferrer">{user.login}</a>
+				</div>
+				<div className={s.follow}>
+					<div className={s.followers}>{user.followers < 1000 ? user.followers : (user.followers / 1000).toFixed(1) + 'k'} followers</div>
+					<div className={s.following}>{user.following < 1000 ? user.following : (user.following / 1000).toFixed(1) + 'k'} following</div>
+				</div>
 			</div>
 		</div>
 	)
